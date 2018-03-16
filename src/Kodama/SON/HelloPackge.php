0d3deedb16b408addbedac8729deead4663a157f
Fernando Kodama <fernando.kodama@tstechbrasil.com.br>
2018-03-16 18:32:25 -0300

    NewFirst commit

---
+++ src/Kodama/SON/HelloPackge.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fernando.kodama
+ * Date: 16/03/2018
+ * Time: 18:08
+ */
+namespace Kodama\SON;
+
+class HelloPackge
+{
+    public function getHello(){
+        return "Hello Package!";
+    }
+}
